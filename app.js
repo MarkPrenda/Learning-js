de6196de8a07f6b70bd68736e4def2c947e9a038
Mark P <markprenda.dev@gmail.com>
2023-05-05 12:53:48 -0300

    alterando o arquivo app

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-console.log("Running the game System")
-console.log("app is connecting to db")
+console.log("Running the game System");
+console.log("app is connecting to db");
+console.log("alteração no arquivo js");
